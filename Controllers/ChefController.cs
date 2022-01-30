adb44802e1577af6bd031a302fec0ae19eacb958
hhanna94 <heatherhanna1994@gmail.com>
2022-01-30 16:54:51 -0500

    CRD for chefs added

--- Controllers/ChefController.cs
+++ Controllers/ChefController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using ChefsNDishes.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,6 +6,11 @@ namespace ChefsNDishes.Controllers
 {
     public class ChefController : Controller
     {
+        private DataContext dbContext;
+        public ChefController(DataContext context) {
+            dbContext = context;
+        }
+
         [HttpGet("chefs/new")]
         public ViewResult ChefForm() {
             return View();
@@ -13,9 +19,19 @@ namespace ChefsNDishes.Controllers
         [HttpPost("/chefs/new")]
         public IActionResult CreateChef(Chef newChef) {
             if (ModelState.IsValid) {
+                dbContext.Add(newChef);
+                dbContext.SaveChanges();
                 return RedirectToAction("Index", "Home");
             }
             return View("ChefForm");
+        }
+
+        [HttpGet("/chefs/{chefId}/delete")]
+        public IActionResult DeleteChef(int chefId) {
+            Chef retrievedChef = dbContext.Chefs.SingleOrDefault(chef => chef.Id == chefId);
+            dbContext.Chefs.Remove(retrievedChef);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,21 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ChefsNDishes.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ChefsNDishes.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private DataContext dbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
             _logger = logger;
+            dbContext = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Chef> allChefs = dbContext.Chefs.Include(chef => chef.CreatedDishes).ToList();
+            return View(allChefs);
         }
 
 
